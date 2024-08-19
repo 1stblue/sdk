c42a4ca845a6431c12f5e9f72f2aac8500fce484
aleafs <zhangxc83@gmail.com>
2024-08-19 12:49:33 +0800

    开发更新

---
+++ java/README.md
@@ -0,0 +1,6 @@
+# Java SDK for bluepipe
+
+## Requirements
+
+* JDK 1.8
+* maven >= 3.9.8
---
+++ java/src/test/java/io/bluepipe/client/core/HttpClientTest.java
@@ -0,0 +1,15 @@
+package io.bluepipe.client.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HttpClientTest {
+
+    private static final String testAddress = "http://localhost/api/v1";
+
+    @Test
+    void shouldAss() {
+        assertEquals(1,1);
+    }
+}
